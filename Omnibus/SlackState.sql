ac7c918138f5d272eb2b95d479a63b31c1a067ec
RobertJBarron <robert.j.barron@gmail.com>
2017-03-15 12:16:45 -0700

    Update SlackState.sql

--- Omnibus/SlackState.sql
+++ Omnibus/SlackState.sql
@@ -6,3 +6,5 @@ insert into alerts.conversions values ('Slack1', 'Slack', 1, 'Queued');
 go
 insert into alerts.conversions values ('Slack2', 'Slack', 2, 'Sent');
 go
+alter table alerts.status add SlackChannel varchar (32);
+go
